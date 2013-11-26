47e761316cca26a9745dc4b20402ce1e5f389793
Jenny Shen <jennyshen.2015@gmail.com>
2013-11-26 13:41:15 -0500

    Added map to listing, redid Listing 'show' view

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,36 +1,10 @@
-//Primary Author: Yinfu
-
-$(document).ready((function() {
-  //initialize listing detail view to be hidden
+$(document).ready(function() {
+  // Initialize listing detail view to be hidden
   $('#listingdetail').slideUp();
   $('#listingdetail').removeClass('listingview');
-
-  //toggle archive/all for feed
-  $('#archivebutton').click(function(){
-    console.log('archivebutton clicked');
-    $(this).hide();
-    $('#feedbutton').show();
-    $.ajax({
-      url: 'display_archive',
-      success: function(result){
-        $('#feedlist').html(result);
-      }
-    });
-    $('#feedbutton').unbind();
-  });
   
-  $('#feedbutton').click(function(){
-    $(this).hide();
-    $('#archivebutton').show();
-    $.ajax({
-      url: 'display_all',
-      success: function(result){
-        $('#feedlist').html(result);
-      }
-    });
-    $('#archivebutton').unbind();
-  });
-
+  // Attach event listeners
+  
   
   //toggle show/hide for favorites
   $('#favsbutton').click(function(){
@@ -57,6 +31,7 @@ $(document).ready((function() {
     $('#listing').removeClass('listing-show').addClass('make_transist').addClass('listing-hide');
     setTimeout(function() {
       $.get( 'listings/' + listing_id, function(data) {
+        makeMap("32-124");
         $('#listing').html(data);
         $('#listing').removeClass('listing-hide').addClass('listing-show');
       });
@@ -65,8 +40,6 @@ $(document).ready((function() {
   
   // favorite/unfavorite listing
   $('.fav-button').click(function(){
-    console.log("fav button clicked");
-    //insert ajax to do following line
     var listing_id = $(this).attr('id').split('-')[2];
     var add_fav = $(this).hasClass('fa-star-o');
     $(this).toggleClass('fa-star-o fa-star');
@@ -83,4 +56,74 @@ $(document).ready((function() {
   $(document).on('ajax:success', '#search_results', function(e, data, status, xhr){
     $('#feedlist').html(xhr.responseText);
   });
+});
+
+/**
+ *$(document).ready((function() {
+  //initialize listing detail view to be hidden
+  $('#listingdetail').slideUp();
+  $('#listingdetail').removeClass('listingview');
+
+  //toggle show/hide for favorites
+  $('#favsbutton').click();
+
+  //show calendar
+  $('#viewcal').click();
+
+  //show listing detail
+  $('.listing-area').click();
+  
+  // favorite/unfavorite listing
+  $('.fav-button').click();
+
+  $(document).on('ajax:success', '#search_results', function(e, data, status, xhr){
+    $('#feedlist').html(xhr.responseText);
+  });
 }));
+
+var showCal = function() {
+  $('#listing').fadeOut(650, $('#calendar').fadeIn(650));
+};
+
+var addFavoriteListing = function() {
+  var listing_id = $(this).attr('id').split('-')[2];
+  var add_fav = $(this).hasClass('fa-star-o');
+  $(this).toggleClass('fa-star-o fa-star');
+  $.ajax({
+    url: "fav_listing",
+    data: "add_fav="+ add_fav + "&listing_id=" + listing_id,
+    success: function(result) {
+      $('#favslist').html(result);
+    }
+  });
+  $(this).unbind();
+};
+
+var showHideFavorites = function(){
+    if ($(this).hasClass('showfavs')) {
+      $('#favslist').slideUp();
+      $(this).removeClass('showfavs');
+      $(this).text('show');
+    } else {
+      $('#favslist').slideDown();
+      $(this).addClass('showfavs');
+      $(this).text('hide');
+    }
+};
+
+var showListing = function(){
+  var listing_id = $(this).attr('id').split('-')[1];
+  $('#listing').removeClass('listing-show').addClass('make_transist').addClass('listing-hide');
+  setTimeout(function() {
+    $.get( 'listings/' + listing_id, function(data) {
+      $('#listing').html(data);
+      $('#listing').removeClass('listing-hide').addClass('listing-show');
+    });
+  }, 300);
+};
+
+var search = function() {
+  
+};
+ 
+ */
