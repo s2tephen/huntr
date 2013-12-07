ad9726aa6d51ab17b26f8124b4f25d9b5bc8eb02
Jenny Shen <jennyshen.2015@gmail.com>
2013-12-07 13:00:58 -0500

    More calendar changes

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -23,3 +23,11 @@ var makeMap = function(location) {
     });
   });
 };
+
+$(document).ready(function() {
+  //show calendar
+  $('#view-cal').click(function(){
+    $('#listing').removeClass('listing-show').addClass('make_transist').addClass('listing-hide');
+    $('#calendar').show();
+  });
+});
