eee737399b8d8c4c266819a3f67a34019e24b0e7
Jenny Shen <jennyshen.2015@gmail.com>
2013-12-08 02:26:55 -0500

    Cal event view

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // prev/next month buttons for calendar
   $('#prev-month').bind('ajax:success', renderCal);
   $('#next-month').bind('ajax:success', renderCal);
-  $('.listing-details').click(showListing);
+  $('.listing-bullet').click(showListing);
 });
 
 var renderCal = function(e, data, status, xhr) {
