31b088a5654dec05780934438e76238a8c13d440
Jenny Shen <jennyshen.2015@gmail.com>
2013-12-08 02:11:20 -0500

    Fix cal

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,10 +1,4 @@
-$(document).ready(function() {
-  //show calendar
-  $('#view-cal').click(function(){
-    $('#listing').removeClass('listing-show').addClass('make_transist').addClass('listing-hide');
-    $('#calendar').show();
-  });
-  
+$(document).ready(function() {  
   // prev/next month buttons for calendar
   $('#prev-month').bind('ajax:success', renderCal);
   $('#next-month').bind('ajax:success', renderCal);
