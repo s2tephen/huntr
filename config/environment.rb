5a5d25a38cedaba33aa73a89332c88ba74b91e32
Stephen Suen <ssuen@mit.edu>
2013-12-08 12:22:28 -0500

    More mailer variables.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Huntr::Application.initialize!
+
+# Set up ActionMailer (for Devise confirmation).
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true }
