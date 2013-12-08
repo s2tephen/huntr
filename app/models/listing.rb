43af39c5361e3925d5c2d266a5f7e45109205142
Stephen Suen <ssuen@mit.edu>
2013-12-08 08:14:08 -0500

    Added basic location parsing, some time parsing fixes - currently only operating at subject line level, change if you so desire.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -16,8 +16,14 @@ class Listing < ActiveRecord::Base
     Mail.find keys: ['NOT', 'SEEN'].each do |m|
       if m.sender == 'eecs-jobs-announce-bounces@lists.csail.mit.edu' # only accept emails from mailman list
         Listing.find_or_create_by(name: m.subject[21..-1]) do |l|
+          begin
+            l.body = m.parts[0].parts[0].decoded
+          rescue => e
+            puts 'error in email: ' + l.name # error message for admin logs
+          end
+
           is_event = false # is true if subject contains a valid date
-          date = l.name.match(/(?<=, )\d{1,2}\/\d{1,2}/).to_s
+          date = l.name.match(/(?<= )\d{1,2}\/\d{1,2}/).to_s
           if date.empty?
             if l.name.match(/today/i)
               date = Date.today.to_s
@@ -34,33 +40,41 @@ class Listing < ActiveRecord::Base
             end
 
             # dat regex — returns two groupings: start time and (optionally) end time
-            time = l.name.match(/(?<=, )(\d{1,2}(?!\/)(?::\d{2})?(?!\d)\s?(?:am|pm)?)(\s?-\s?\d{1,2}(?::\d{2})?(?!\d)\s?(?:am|pm)?)?/i)
+            time = l.name.match(/(?<= )(\d{1,2}(?!\/)(?::\d{2})?(?!\d)\s?(?:am|pm)?)(\s?-\s?\d{1,2}(?::\d{2})?(?!\d)\s?(?:am|pm)?)?/i)
             unless time.nil?
               if time[2].nil? # start time only
                 l.start_time = DateTime.parse(date + ' ' + time[1], :datetime)
                 l.end_time = l.start_time + 1.hour # default to 1 hour
               else # end time specified
                 cleaned_end = time[2].split('-')[1].lstrip # remove leading hyphen/whitespace
-                l.end_time = DateTime.parse(date + ' ' + cleaned_end, :datetime)
-                if time[1].match(/am|pm/i).nil?
-                  l.start_time = DateTime.parse(date + ' ' + time[1] + cleaned_end.match(/am|pm/i).to_s, :datetime) # e.g. 1-2pm
+                if cleaned_end.match(/am|pm/i).nil?
+                  l.end_time = DateTime.parse(date + ' ' + cleaned_end + 'pm', :datetime) # assume pm when unspecified
                 else
-                  l.start_time = DateTime.parse(date + ' ' + time[1], :datetime) # e.g. 11am-1pm
+                  l.end_time = DateTime.parse(date + ' ' + cleaned_end, :datetime)
+                end
+                if time[1].match(/am|pm/i).nil?
+                  l.start_time = DateTime.parse(date + ' ' + time[1] + cleaned_end.match(/am|pm/i).to_s, :datetime)
+                  l.start_time -= 12.hours if l.start_time > l.end_time # e.g. 11-3pm => 11pm-3pm => 11am-3pm
+                else
+                  l.start_time = DateTime.parse(date + ' ' + time[1], :datetime)
                 end
               end
               # timezone shift
               l.start_time += 5.hours
               l.end_time += 5.hours
             end
+
+            # location parsing - very basic
+            l.location = l.name.match(/(?<= )[ENW]{0,2}\d{1,3}[A-G]?-\d{3,4}/).to_s
+            if l.location.empty? # other location names
+              ['Student Center', 'Kresge', 'Walker Memorial', 'Religious Activities Center',
+               'Lobby 13', 'MIT Chapel', 'Lobby 10', 'Wong Auditorium', 'Stata'].each do |loc|
+                  l.location = l.name.match(/#{loc}/).to_s unless l.name.match(/#{loc}/).nil?
+              end
+            end
           end
 
           l.updated_at = m.date # use date/time of email
-
-          begin
-            l.body = m.parts[0].parts[0].decoded
-          rescue => e
-            puts 'error in email: ' + l.name # error message
-          end
 
           # TODO: category
           # TODO: food
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,18 @@ Mail.defaults do
 end
 
 # TODO: update this any time Listing.fetch is updated
-puts 'Seeding db (fetching all mail) — this could take a while!'
+puts 'Seeding db — this could take a while!'
 
 Mail.all.each do |m|
   if m.sender == 'eecs-jobs-announce-bounces@lists.csail.mit.edu' # only accept emails from mailman list
     Listing.find_or_create_by(name: m.subject[21..-1]) do |l|
-      puts '  ' + l.name
+      puts '-- ' + l.name
+      begin
+        l.body = m.parts[0].parts[0].decoded
+      rescue => e
+        puts '   -> error fetching!' # error message for admin logs
+      end
+
       is_event = false # is true if subject contains a valid date
       date = l.name.match(/(?<= )\d{1,2}\/\d{1,2}/).to_s
       if date.empty?
@@ -33,6 +39,7 @@ Mail.all.each do |m|
       else
         is_event = true
       end
+      puts '   -> date: ' + date unless date.empty?
 
       if is_event
         # round to next year if date has passed
@@ -41,35 +48,44 @@ Mail.all.each do |m|
         end
 
         # dat regex — returns two groupings: start time and (optionally) end time
-        time = l.name.match(/(?<=, )(\d{1,2}(?!\/)(?::\d{2})?(?!\d)\s?(?:am|pm)?)(\s?-\s?\d{1,2}(?::\d{2})?(?!\d)\s?(?:am|pm)?)?/i)
+        time = l.name.match(/(?<= )(\d{1,2}(?!\/)(?::\d{2})?(?!\d)\s?(?:am|pm)?)(\s?-\s?\d{1,2}(?::\d{2})?(?!\d)\s?(?:am|pm)?)?/i)
         unless time.nil?
           if time[2].nil? # start time only
             l.start_time = DateTime.parse(date + ' ' + time[1], :datetime)
             l.end_time = l.start_time + 1.hour # default to 1 hour
           else # end time specified
             cleaned_end = time[2].split('-')[1].lstrip # remove leading hyphen/whitespace
-            l.end_time = DateTime.parse(date + ' ' + cleaned_end, :datetime)
-            if time[1].match(/am|pm/i).nil?
-              l.start_time = DateTime.parse(date + ' ' + time[1] + cleaned_end.match(/am|pm/i).to_s, :datetime) # e.g. 1-2pm
+            if cleaned_end.match(/am|pm/i).nil?
+              l.end_time = DateTime.parse(date + ' ' + cleaned_end + 'pm', :datetime) # assume pm when unspecified
             else
-              l.start_time = DateTime.parse(date + ' ' + time[1], :datetime) # e.g. 11am-1pm
+              l.end_time = DateTime.parse(date + ' ' + cleaned_end, :datetime)
+            end
+            if time[1].match(/am|pm/i).nil?
+              l.start_time = DateTime.parse(date + ' ' + time[1] + cleaned_end.match(/am|pm/i).to_s, :datetime)
+              l.start_time -= 12.hours if l.start_time > l.end_time # e.g. 11-3pm => 11pm-3pm => 11am-3pm
+            else
+              l.start_time = DateTime.parse(date + ' ' + time[1], :datetime)
             end
           end
           # timezone shift
           l.start_time += 5.hours
           l.end_time += 5.hours
-          puts '    start_time: ' + l.start_time.to_s
-          puts '    end_time: ' + l.end_time.to_s
+          puts '   -> start_time: ' + l.start_time.strftime("%I:%M%p") unless l.start_time.nil?
+          puts '   -> end_time: ' + l.end_time.strftime("%I:%M%p") unless l.end_time.nil?
         end
+
+        # location parsing - very basic
+        l.location = l.name.match(/(?<= )[ENW]{0,2}\d{1,3}[A-G]?-\d{3,4}/).to_s
+        if l.location.empty? # other location names
+          ['Student Center', 'Kresge', 'Walker Memorial', 'Religious Activities Center',
+           'Lobby 13', 'MIT Chapel', 'Lobby 10', 'Wong Auditorium', 'Stata'].each do |loc|
+              l.location = l.name.match(/#{loc}/).to_s unless l.name.match(/#{loc}/).nil?
+          end
+        end
+        puts '   -> location: ' + l.location unless l.location.empty?
       end
 
       l.updated_at = m.date # use date/time of email
-
-      begin
-        l.body = m.parts[0].parts[0].decoded
-      rescue => e
-        puts 'error in email: ' + l.name # error message
-      end
 
       # TODO: category
       # TODO: food
