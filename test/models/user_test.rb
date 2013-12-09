7cff608eac3a642290d9e6b5973fef8150005d3e
sylviaz <sylviaz@mit.edu>
2013-12-08 21:15:33 -0500

    Added tests for query_listings method.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,4 +23,21 @@ class UserTest < ActiveSupport::TestCase
     	@user.favorite(@listing)
     end
   end
+
+  test "query listings" do
+    #filter by category "urop"
+    l = @user.query_listings("urop", "")
+    assert_equal(l.count, 1, 'ERROR: query should only return 1 listing')
+    assert_equal(l[0].category, "urop", 'ERROR: category should be urop')
+
+    #filter by category "MyString"
+    l = @user.query_listings("MyString", "")
+    assert_equal(l.count, 2, 'ERROR: query should 2 listings')
+    l.each {|x| assert_equal(x.category, "MyString", 'ERROR: category should be MyString')}
+
+    #filter by category "MyString" and search for "MyText2"
+    l = @user.query_listings("MyString", "MyText2")
+    assert_equal(l.count, 1, 'ERROR: query should only return 1 listing')
+    assert_equal(l[0].category, "MyString", 'ERROR: category should be MyString')
+  end
 end
