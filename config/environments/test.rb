e76eb9c6809f78470c6190820bec332025e6228a
sylviaz <sylviaz@mit.edu>
2013-12-09 01:31:29 -0500

    Added tests for the fetch function.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,11 @@
 Huntr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.before_configuration do
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(env_file)
+  end
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
